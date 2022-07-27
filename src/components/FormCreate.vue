1ec9178a5c4b9e9b34f1ae49c61880bbaea4e502
ioanlee <reddaravus@gmail.com>
2022-07-27 20:12:06 +1000

    router wip

--- src/components/FormCreate.vue
+++ src/components/FormCreate.vue
@@ -3,11 +3,15 @@
 	export default {		
 		data() {
 			return {
+				file1: 1,
+				file2: null,
+				value: '',
 				form: {
 					name: "",
 					image: "./assets/logo.png",
 					category: null,
 				},
+				categories: null,
 				options: [
 					{ value: null, text: "Please select an option" },
 					{ value: "a", text: "This is First option" },
@@ -15,14 +19,18 @@
 					{ value: { C: "3PO" }, text: "This is an option with object value" },
 					{ value: "d", text: "This one is disabled" }
 				],
-				file1: 1,
-				file2: null,
-				value: ''
 			};
 		},
 		methods: {
 			onSubmit() { alert(JSON.stringify(this.form)) },
+			async getCategories() {
+				// await fetch('https://jsonplaceholder.typicode.com/todos/1')
+				// 	.then(response => response.json())
+				// 	.then(json => this.categories = json)
+				await fetch('http://localhost:')
+			}
 		},
+		async mounted() {	}
 	}
 </script>
 
@@ -38,7 +46,7 @@
 		/>
 		<div class="w-50 m-auto text-start">Выберите категорию</div>
 		<b-form-select
-			:options="options"
+			:options="categories"
 			v-model="form.category" 
 			class="w-50 m-auto mb-3" 
 		/>
@@ -58,5 +66,13 @@
 				innerText="Оформить блюдо"
 			/>
 		</div>
+		<hr>
+
+		<b-button
+			innerText="fetch"
+			v-on:click="getCategories"
+		/>
+		<div>{{ categories }}</div>
+
 	</b-form>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import App from './App.vue'
-import router from './router'
+import router from './routes/router'
 import { createApp } from 'vue'
 import BootstrapVue3 from 'bootstrap-vue-3'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+// .use(router)
 createApp(App)
-	.use(router)
 	.use(BootstrapVue3)
 	.mount('#app')
